446e85ff3bf02380f062716ea3aa629d5b63fc85
crissandu <85240660+crissandu@users.noreply.github.com>
2024-09-10 20:00:35 +0300

    Sm tm c38c9 (#2)
    * Initial commit
    
    * fixing conflicts
    
    * tema
    
    * tema
    
    * tema
    
    * tema
    
    * tema
    
    * tema
    
    * tema
    
    * tema
    
    * initial commit
    
    ---------
    
    Co-authored-by: GeorgeCodreanu <56363538+GeorgeCodreanu@users.noreply.github.com>
    Co-authored-by: GeorgeCodreanu <george.codreanu1991@gmail.com>

---
+++ src/test/java/OtherToolsTest.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class OtherToolsTest {
+
+    private WebDriver driver;
+    private static final String URL_HOME_PAGE ="https://practicesoftwaretesting.com/";
+    private static final String SPECIAL_TOOLS_PAGE ="https://practicesoftwaretesting.com/category/special-tools";
+
+
+    @BeforeMethod
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--disable-search-engine-choice-screen");
+        options.addArguments("--window-size=1280,1024");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://practicesoftwaretesting.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void otherToolsTest() {
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.equals("https://practicesoftwaretesting.com/"));
+        Assert.assertEquals(currentUrl, URL_HOME_PAGE);
+
+        WebElement categories = driver.findElement(By.xpath("//a[@data-test='nav-categories']"));
+        categories.click();
+        driver.findElement(By.xpath("//a[text()='Special Tools']")).click();
+        String specialToolsUrl = driver.getCurrentUrl();
+        Assert.assertEquals(specialToolsUrl, SPECIAL_TOOLS_PAGE);
+
+
+    }
+
+
+}
