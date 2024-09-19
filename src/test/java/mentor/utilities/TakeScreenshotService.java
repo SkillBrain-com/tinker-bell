09ba7c54d89d8f4b437fe1632b84f016480fe9af
crissandu <85240660+crissandu@users.noreply.github.com>
2024-09-19 20:43:44 +0300

    Window handles and take screenshot examples (#9)

---
+++ src/test/java/mentor/utilities/TakeScreenshotService.java
@@ -0,0 +1,33 @@
+package mentor.utilities;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class TakeScreenshotService {
+    
+    
+    public static void takeScreenshot(WebDriver driver) {
+        String destinationFile = null;
+//                                      CAST (TRANSFORMATION)
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+        destinationFile = System.getProperty("user.dir")  +"/src/test/java/mentor/testResults/Screenshot-" + LocalDateTime.now() + ".png";
+        try {
+            FileUtils.copyFile(source, new File(destinationFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    
+    
+    
+    
+}
