2851fad2dc0ffba41ea63256bc720d9a1408fd43
sandu cristian <sandu_cristian2007@yahoo.com>
2024-09-26 21:06:51 +0300

    selenium 3 demo

--- src/test/java/mentor/pageObject/AlertPageObject.java
+++ src/test/java/mentor/pageObject/AlertPageObject.java
@@ -5,22 +5,31 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class AlertPageObject {
+public class AlertPageObject extends BasePage {
 
-//TODO - please extend BasePage and use its constructor to initialize the object (hint Check BrowserWindowPageObject)
 
     public static final String ALERT_PAGE_URL = "https://demoqa.com/alerts";
 
-    public AlertPageObject(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-    }
+    @FindBy(id = "timerAlertButton")
+    private WebElement timerAlertButton;
 
+    public WebElement getTimerAlertButton() {
+        return timerAlertButton;
+    }
 
     @FindBy(id = "alertButton")
     private WebElement alertButton;
 
     @FindBy(id = "confirmButton")
     private WebElement confirmButton;
+
+    public AlertPageObject(WebDriver driver) {
+        super(driver);
+    }
+
+    public void goToAlertPage() {
+        driver.get(ALERT_PAGE_URL);
+    }
 
 
     public WebElement getAlertButton() {
---
+++ src/test/java/mentor/pageObject/CheckBoxPageObject.java
@@ -0,0 +1,11 @@
+package mentor.pageObject;
+
+public class CheckBoxPageObject {
+
+//    TODO - build page object model
+//    TODO - create selectors
+//    TODO - Land of https://demoqa.com/checkbox
+//    TODO - Select React from workspace
+//    TODO - Assert React is selected !!
+
+}
