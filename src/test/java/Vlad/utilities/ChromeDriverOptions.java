d08416e37908376454a716f22d2636941b68bf9f
Vlad-02 <vladutzbratu@yahoo.com>
2024-10-08 19:57:11 +0300

    homework (#17)

---
+++ src/test/java/Vlad/utilities/ChromeDriverOptions.java
@@ -0,0 +1,17 @@
+package Vlad.utilities;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeDriverOptions {
+
+    public static ChromeOptions getChromeOptions(){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--disable-search-engine-choice-screen");
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        return options;
+    }
+}
---
+++ src/test/java/Vlad/utilities/FirefoxDriverOption.java
@@ -0,0 +1,16 @@
+package Vlad.utilities;
+
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class FirefoxDriverOption {
+
+    public static FirefoxOptions getFirefoxOptions() {
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--disable-search-engine-choice-screen");
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        return options;
+    }
+}
